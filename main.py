4ef5d72e54b69a1ca7ae8333f4512a5a0d896e60
Andrew Kempen <akemperfi@gmail.com>
2016-02-01 21:52:42 -0600

    Finished basic tower code and added FPS meter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import os
 import numpy as np
 import networktables
@@ -7,22 +8,28 @@ TESTMODE = True
 MANUALIMAGEMODE = True
 towerCameraRes = [1280.0, 720.0]
 ballCameraRes = [1280.0, 720.0]
-imageNumber = 2
-
+imageNumber = 1
+#
 def processTowerCamera(camera):
     filteredContours = []
     originalImage = pollCamera(camera)
     blurredImage = cv2.blur(originalImage, (6, 6))
     RGBImage = cv2.cvtColor(blurredImage, cv2.COLOR_BGR2RGB)
     RBGThresholdImage = cv2.inRange(RGBImage, np.array([69,122,197]), np.array([255, 255, 255]))
-    _,contours,hierarchy = cv2.findContours(RBGThresholdImage, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _,contours,_ = cv2.findContours(RBGThresholdImage, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for x in contours:
-        if cv2.contourArea(x) < 2000 and cv2.contourArea(x) > 1200:
-            filteredContours.append(x)
-    cv2.drawContours(RBGThresholdImage,filteredContours,-1,(255,255,255),3)
-    cv2.imshow("output", RBGThresholdImage)
+        area = cv2.contourArea(x)
+        hull = cv2.convexHull(x)
+        if area != 0:
+            hull_area = cv2.contourArea(hull)
+            solidity = float(area)/hull_area
+            if (solidity < 0.5 and area > 1200):
+                filteredContours.append(x)
+    cv2.drawContours(originalImage,filteredContours,-1,(0,0,255),3)
+    if(TESTMODE):
+        cv2.imshow("contours", originalImage)
+    #TODO publish contours to network tables
     #end def
-
 def processBallCamera(camera):
     originalImage = pollCamera(camera)
     originalImage
@@ -40,14 +47,24 @@ def pollCamera(camera):
     #end else
     return imgOriginal
 #end def
+
+def calculateFPS(lastTime):
+    currentTime = time.struct_time.tm_sec
+    fps = (currentTime - lastTime)
+    print "FPS: " + str(fps)
+    return currentTime
+#end def
         
 def main():
-    towerCamera = cv2.VideoCapture(0)
-    #ballCamera = cv2.VideoCapture(1)
+    if MANUALIMAGEMODE == True:
+        towerCamera = cv2.VideoCapture(0)
+        #ballCamera = cv2.VideoCapture(1)
+    #end if
     towerCamera.set(cv2.CAP_PROP_FRAME_WIDTH, towerCameraRes[0])
     towerCamera.set(cv2.CAP_PROP_FRAME_HEIGHT, towerCameraRes[1])
     print "Tower Camera Resolution = " + str(towerCamera.get(cv2.CAP_PROP_FRAME_WIDTH)) + "x" + str(towerCamera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+    lastTime = time.struct_time.tm_sec
+    fps = 1
     if towerCamera.isOpened() == False:
         print "error: Tower Camera not initialized"
         os.system("pause")
@@ -56,6 +73,7 @@ def main():
     
     while cv2.waitKey(1) != 27 and towerCamera.isOpened(): #and ballCamera.isOpened()
         processTowerCamera(towerCamera)
+        lastTime = calculateFPS(lastTime)
         #end if
     # end while
     cv2.destroyAllWindows()
