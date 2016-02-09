54065f4d4fdd2f341ccf7798957671d276d77231
Andrew Kempen <akemperfi@gmail.com>
2016-02-09 15:19:16 -0600

    Added broken code to test circle detection

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 import platform
 from networktables import NetworkTable
+from cv2 import circle
 
 TESTMODE = True
 MANUALIMAGEMODE = True
@@ -16,6 +17,15 @@ frameNumber = 0
 frames = 0
 visionNetworkTable = None
 hostname = "roboRIO-2062-FRC.local"
+
+#######################
+# Temp
+min_dist = 100
+param_1 = 100
+param_2 = 100
+min_radius = 20
+max_radius = 0
+#######################
 
 def processTowerCamera(camera):
     filteredContours = []
@@ -47,8 +57,20 @@ def processTowerCamera(camera):
         cv2.imshow("Image", originalImage)
 #end def
 def processBallCamera(camera):
+    greyImage = None
+    circles = None
     originalImage = pollCamera(camera)
-    originalImage
+    greyImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    greyImage = cv2.blur(greyImage, (15, 15))
+    circles = cv2.HoughCircles(greyImage,cv2.HOUGH_GRADIENT,1,min_dist,param_1,param_2,min_radius,max_radius)
+    circles = np.uint16(np.around(circles))
+    if len(circles) != 0:
+        for i in circles[0,:]:
+            cv2.circle(originalImage,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(originalImage,(i[0],i[1]),2,(0,0,255),3)
+    if(TESTMODE):
+        cv2.imshow("Image", originalImage)
+    return originalImage
 #end def
 def sendNumber(name, value):
     if(TESTMODE):
@@ -57,7 +79,8 @@ def sendNumber(name, value):
 #end def
 def pollCamera(camera):
     if MANUALIMAGEMODE == True:
-        imgOriginal = cv2.imread('towerImages/tower (' + str(imageNumber) + ')_960x540.jpg',1)
+        #imgOriginal = cv2.imread('towerImages/tower (' + str(imageNumber) + ')_960x540.jpg',1)
+        imgOriginal = cv2.imread('boulderImages/boulder (' + str(imageNumber) + ').jpg',1)
     else:
         blnFrameReadSuccessfully, imgOriginal = camera.read()
         if not blnFrameReadSuccessfully or imgOriginal is None:
@@ -89,6 +112,22 @@ def calculateFPS(lastTime):
 def changeImage(img):
     global imageNumber
     imageNumber = img+1
+def change_min_dist(img):
+    global min_dist
+    min_dist = img
+def change_param_1(img):
+    global param_1
+    param_1 = img
+def change_param_2(img):
+    global param_2
+    param_2 = img
+def change_min_radius(img):
+    global min_radius
+    min_radius = img
+def change_max_radius(img):
+    global max_radius
+    max_radius = img
+#min_dist,param_1,param_2,min_radius,max_radius
 def main():
     if platform.system() == "Linux":
         global TESTMODE
@@ -113,8 +152,14 @@ def main():
     if TESTMODE:
         cv2.namedWindow("Image",cv2.WINDOW_AUTOSIZE)
         cv2.createTrackbar("Image #", "Image", 0, 37, changeImage)
+        cv2.createTrackbar("min_dist", "Image", 50, 100, change_min_dist)
+        cv2.createTrackbar("param_1", "Image", 100, 200, change_param_1)
+        cv2.createTrackbar("param_2", "Image", 100, 100, change_param_2)
+        cv2.createTrackbar("min_radius", "Image", 1, 100, change_min_radius)
+        cv2.createTrackbar("max_radius", "Image", 0, 100, change_max_radius)
     while cv2.waitKey(1) != 27 and towerCamera.isOpened(): #and ballCamera.isOpened()
-        processTowerCamera(towerCamera)
+        #processTowerCamera(towerCamera)
+        processBallCamera(towerCamera)
         lastTime = calculateFPS(lastTime)
     cv2.destroyAllWindows()
     visionNetworkTable.putString("debug", strftime("%H:%M:%S", gmtime())+": Program End")
