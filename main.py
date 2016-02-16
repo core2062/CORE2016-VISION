047872a112831c516db4695e65cc7b27fa618983
Andrew Kempen <akemperfi@gmail.com>
2016-02-16 12:54:00 -0600

    Cleaned up code

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ MANUALIMAGEMODE = True
 FILTERTYPE = "HSV" #"RGB" "HSL"
 IMAGESOURCE = 'idealTower'
 
+#Possible Wide Screen Resolutions: 1280x720(921,600), 960x544(522,240), 800x448(358,400), 640x360(230,400)
 towerCameraRes = [960, 544]
-ballCameraRes = [1280.0, 720.0]
+ballCameraRes = [800, 448]
 imageNumber = 1
 frameNumber = 0
 frames = 0
@@ -23,7 +24,7 @@ visionNetworkTable = None
 hostname = "roboRIO-2062-FRC.local"
 
 def recordVideo(cap):
-    video  = cv2.VideoWriter(('towerVideos/towerCamera'+str(strftime("%m-%d_%H.%M", gmtime()))+'.avi'), cv2.VideoWriter_fourcc('X','V','I','D'), 14.0, (int(towerCameraRes[0]), int(towerCameraRes[1])));
+    video = cv2.VideoWriter(('towerVideos/towerCamera'+str(strftime("%m-%d_%H.%M", gmtime()))+'.avi'), cv2.VideoWriter_fourcc('X','V','I','D'), 14.0, (int(towerCameraRes[0]), int(towerCameraRes[1])))
     global lastTime
     lastTime = time.clock()
     while True:
@@ -52,15 +53,18 @@ def processTowerCamera(camera):
     for x in contours:
         centroid,size,angle = cv2.minAreaRect(x)
         area = abs(size[0]*size[1])
-        hull = cv2.convexHull(x)
-        contourArea = cv2.contourArea(x)
         if area != 0:
+            hull = cv2.convexHull(x)
+            contourArea = cv2.contourArea(x)
             hull_area = cv2.contourArea(hull)
             solidity = float(contourArea)/hull_area
-            if (solidity < 0.4 and area/(towerCameraRes[0]*towerCameraRes[1]) > 0.001714 and size[1]/(towerCameraRes[0]*towerCameraRes[1]) > 0.00008573):
+            cameraPixels = (towerCameraRes[0]*towerCameraRes[1])
+            if (solidity < 0.4 and area/cameraPixels > 0.001714 and size[1]/cameraPixels > 0.00008573):
                 if(area > largestArea):
                     largestContour = centroid,size,angle,x
+                    largestArea = area
                 filteredContours.append(x)
+    cv2.drawContours(originalImage,contours,-1,(0,255,255),2)
     cv2.drawContours(originalImage,filteredContours,-1,(0,0,255),2)
     if largestContour is not None:
         sendNumber("goal_x", round(largestContour[0][0],1))
@@ -68,32 +72,25 @@ def processTowerCamera(camera):
         sendNumber("goal_width", round(largestContour[1][0],1))
         sendNumber("goal_height", round(largestContour[1][1],1))
         sendNumber("goal_angle", round(largestContour[2],1))
-        cv2.circle(originalImage, (int(largestContour[0][0]),int(largestContour[0][1])), 2, np.array([0,255,0]), 5, 2);
+        cv2.circle(originalImage, (int(largestContour[0][0]),int(largestContour[0][1])), 2, np.array([0,255,0]), 5, 2)
     if(TESTMODE):
         cv2.imshow("Image", originalImage)
-#end def
 def processBallCamera(camera):
     originalImage = pollCamera(camera)
-    originalImage
-#end def
+    return originalImage
 def sendNumber(name, value):
-    if(TESTMODE):
+    if DEBUGMODE == True:
         print name + ": " + str(value)
     visionNetworkTable.putNumber(name,value)
-#end def
 def pollCamera(camera):
     if MANUALIMAGEMODE == True:
-        imgOriginal = cv2.imread('towerImages/tower (' + str(imageNumber) + ')_960x540.jpg',1)
+        imgOriginal = cv2.imread('towerImages/' + IMAGESOURCE + '/' + IMAGESOURCE +' (' + str(imageNumber) + ')_960x540.jpg',1)
     else:
         blnFrameReadSuccessfully, imgOriginal = camera.read()
         if not blnFrameReadSuccessfully or imgOriginal is None:
             print "error: frame not read from towerCamera"
             return
-        #end if
-    #end else
     return imgOriginal
-#end def
-
 def calculateFPS(lastTime):
     currentTime = time.clock()
     global frameNumber
@@ -110,8 +107,6 @@ def calculateFPS(lastTime):
         frames += 1.0
         frameNumber+=1
         return lastTime
-#end def
-
 def changeImage(img):
     global imageNumber
     imageNumber = img+1
@@ -135,7 +130,7 @@ def main():
     towerCameraRes[0] = towerCamera.get(cv2.CAP_PROP_FRAME_WIDTH)
     towerCameraRes[1] = towerCamera.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print "Tower Camera Resolution = " + str(towerCameraRes[0]) + "x" + str(towerCameraRes[1])
-    visionNetworkTable.putString("debug", (strftime("%H:%M:%S", gmtime())+": Camera Res = " + str(towerCamera.get(cv2.CAP_PROP_FRAME_WIDTH)) + "x" + str(towerCamera.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    visionNetworkTable.putString("debug", (strftime("%H:%M:%S", gmtime())+": Camera Res = " + str(towerCameraRes[0]) + "x" + str(towerCameraRes[1])))
     lastTime = time.clock()
     if towerCamera.isOpened() == False:
         print "error: Tower Camera not initialized"
@@ -146,7 +141,8 @@ def main():
     else:
         if TESTMODE:
             cv2.namedWindow("Image",cv2.WINDOW_AUTOSIZE)
-            cv2.createTrackbar("Image #", "Image", 0, 37, changeImage)
+            if MANUALIMAGEMODE:
+                cv2.createTrackbar("Image #", "Image", 0, 54, changeImage)
         while cv2.waitKey(1) != 27 and towerCamera.isOpened(): #and ballCamera.isOpened()
             if visionNetworkTable.getString("mode", "tower") == "tower":
                 processTowerCamera(towerCamera)
